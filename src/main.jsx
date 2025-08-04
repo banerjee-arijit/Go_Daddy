81437b8af19e88e15ef81c6781f67432a5f53c69
Arijit Banerjee <arijitbanerjee873@gmail.com>
2025-08-04 12:57:58 +0530

    restart

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { StrictMode } from "react";
-import router from "./router/routes";
-import { RouterProvider } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const container = document.getElementById("root");
-
-if (!container) throw new Error("404!Root is Not Present!");
-
-const root = createRoot(container);
-
-root.render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
--- src/router/routes.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { createBrowserRouter } from "react-router-dom";
-import LandingPage from "../pages/LandingPage";
-
-const router = createBrowserRouter([{ path: "/", element: <LandingPage /> }]);
-
-export default router;
